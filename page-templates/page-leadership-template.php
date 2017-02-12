883c59a31a039eca6a335db5950114740eb4d33a
Emilio Garza <emilio@dev-e.com>
2017-02-12 02:24:34 -0800

    Some tweaking of things, getting started on Team page

--- page-templates/page-leadership-template.php
+++ page-templates/page-leadership-template.php
@@ -1,18 +1,35 @@
 <?php
 /*
-Template Name: Full Width
+Template Name: Leadership
 */
 get_header(); ?>
-<div id="page-full-width" role="main">
+
+<?php
+
+      if(get_field('hero_image')) {
+        echo '<img src="'. get_field("hero_image") .'" class="event-hero" />';
+      }
+
+      if(get_field('cover_image')) {
+        echo '
+        <div class="text-center">
+          <div class="image-frame full-width">
+            <img src="'. get_field("cover_image") .'" />
+          </div>
+        </div>';
+      }
+
+?>
+<div id="leadership-page" role="main">
 
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
   <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-      <header>
+      <header class="text-center">
           <h1 class="entry-title"><?php the_title(); ?></h1>
       </header>
       <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-      <div class="entry-content">
+      <div class="entry-content text-center">
           <?php the_content(); ?>
 
       </div>
