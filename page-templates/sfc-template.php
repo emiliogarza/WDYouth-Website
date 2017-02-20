e2526fb5f2fe281e4bec2ad660b8b6356bc30bc6
Emilio Garza <emilio@dev-e.com>
2017-02-19 23:39:12 -0800

    Made my sfc page content manageable

--- page-templates/sfc-template.php
+++ page-templates/sfc-template.php
@@ -20,66 +20,75 @@ get_header(); ?>
       <div class="entry-content">
           <?php the_content(); ?>
       </div>
-
   <div class="row">
     <div class="small-12 medium-4 columns image">
-        <img class="anime" src="http://wdyouth.org/wp-content/themes/wdyouth16/assets/images/sfc-ministries.png" />
+      <?php
+        if(get_field('sfc_ministries_image')) {
+          echo '<img class="anime" src="'. get_field("sfc_ministries_image") .'" />';
+        }
+      ?>    
     </div>
     <div class="small-12 medium-8 columns content">
       <h2>Sheaves for Christ Ministries</h2>
-      <p>
-          Sheaves for Christ funds missions globally, including in our own district. Sheaves for Christ dollars make a huge world impact! Below are some of the ministries SFC supports. Find out more information about these great ministries at <a href="http://sheavesforchrist.com/sfc-is/" target="_blank">sheavesforchrist.com/sfc-is/</a>.
-      </p>
-      <p>
-        <strong>Global Missions</strong> - <strong>North American Missions</strong> - <strong>Tupelo Children's Mansion</strong> - <strong>Lighthouse Ranch for Boys</strong> - <strong>Senior Bible Quizzing</strong> - <strong>Spirit of Freedom Ministries</strong> - <strong>Divisions of Education</strong> - <strong>Apostolic Youth Corps</strong> - <strong>Campus Ministry International</strong> - <strong>Hyphen Young Adult Ministry</strong> - <strong>District Projects</strong>
-      </p>
+      <?php
+        if(get_field('sfc_ministries_content')) {
+          echo get_field("sfc_ministries_content");
+        }
+      ?> 
     </div>
   </div>
 
   <div class="row">
     <div class="small-12 medium-4 medium-push-8 columns image">
-        <img class="anime" src="http://wdyouth.org/wp-content/themes/wdyouth16/assets/images/walkathon.png" />
+      <?php
+        if(get_field('sfc_goldengate_image')) {
+          echo '<img class="anime" src="'. get_field("sfc_goldengate_image") .'" />';
+        }
+      ?> 
     </div>   
     <div class="small-12 medium-8 medium-pull-4 columns content">
       <h2>Golden Gate Walkathon</h2>
-      <p>
-          On <strong>August 20th, 2016</strong> the young people of the Western District will walk the golden gate bridge to raise money for SFC! Orientation will begin at 10am at the pavilion. <br />Start getting sponsors today by downloading the Sponsor Sheet!
-      </p>
-      <a role="button" class="download small-12 medium-4 large button" href="/wp-content/uploads/2016/05/SponsorSheet_2016.pdf" target="_blank">Sponsor Sheet</a>
-
+      <?php
+        if(get_field('sfc_goldengate_content')) {
+          echo get_field("sfc_goldengate_content");
+        }
+      ?>
     </div>   
   </div>
 
 
   <div class="row">
     <div class="small-12 medium-4 columns image">
-        <img class="anime" src="http://wdyouth.org/wp-content/themes/wdyouth16/assets/images/c31_logo.png" />
+      <?php
+        if(get_field('sfc_c31_image')) {
+          echo '<img class="anime" src="'. get_field("sfc_c31_image") .'" />';
+        }
+      ?>
     </div>
     <div class="small-12 medium-8 columns content">
       <h2>Challenge 31</h2>
-      <p>
-        C31 is an opportunity for students to raise between $341-$961 for Sheaves for Christ
-by finding sponsors who will give $11, $21, or $31. Participants could choose to set a
-goal of 1 sponsor a day for a month (31 days in August).
-      </p>
-      <a role="button" class="download small-12 medium-4 large button c31" href="http://download.pentecostalyouth.org/SFC/2016/C31_Student_Guide.pdf" target="_blank">Sponsor Sheet</a>
-      <a role="button" class="download small-12 medium-4 large button c31" href="http://download.pentecostalyouth.org/SFC/2016/C31_Leader_Guide.pdf" target="_blank">Leaders Guide</a>
+      <?php
+        if(get_field('sfc_c31_content')) {
+          echo get_field("sfc_c31_content");
+        }
+      ?>    
     </div>
   </div>
   <div class="row">
     <div class="small-12 medium-4 medium-push-8 columns image">
-        <img class="anime" src="http://wdyouth.org/wp-content/themes/wdyouth16/assets/images/mccoy.jpg" />
+      <?php
+        if(get_field('sfc_realmccoy_image')) {
+          echo '<img class="anime" src="'. get_field("sfc_realmccoy_image") .'" />';
+        }
+      ?>    
     </div>   
     <div class="small-12 medium-8 medium-pull-4 columns content">
       <h2>The Real McCoy</h2>
-      <p>
-        Many young people have discovered their passion for God’s Kingdom by getting personally involved in life-giving SFC fundraising via The Real McCoy Contest. The students in your church can be involved in this exciting contest as we challenge our Apostolic youth to live sacrificially and become a generation of givers.
-      </p>
-      <p>
-Winners from each district across North America will be flown in to the World Evangelism Center in St. Louis, MO, for an exclusive weekend with the executives of the General Youth Division. For more details please visit <a href="http://sheavesforchrist.com/the-real-mccoy-contest/" target="_blank">sheavesforchrist.com</a> or apply below!
-      </p>
-
-      <a role="button" class="download small-12 medium-4 large button c31" href="http://download.pentecostalyouth.org/SFC/RM/Real_McCoy_Contest_Application_Fillable.pdf" target="_blank">Application</a>
+      <?php
+        if(get_field('sfc_realmccoy_content')) {
+          echo get_field("sfc_realmccoy_content");
+        }
+      ?>  
     </div>   
   </div>
     
